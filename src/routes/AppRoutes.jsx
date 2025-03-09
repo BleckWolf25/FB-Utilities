86b4bc3c5cdb8a1f59c3e266510571b8d5994590
BleckWolf25 <joao.coutinho08@icloud.com>
2025-03-09 17:01:20 +0000

    App Routes Update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,23 +1,19 @@
 // src/routes/AppRoutes.jsx
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-// Import layout components
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
-
-// Import page components
 import Home from '../pages/Home';
-// import About from '../pages/About';
-// import Contact from '../pages/Contact';
+import About from '../pages/About';
 import NotFound from '../pages/NotFound';
+import Converter from '../pages/Converter';
+import Minifier from '../pages/Minifier'; // Import the Minifier component
 
 // Layout wrapper component that includes navbar and footer
 const MainLayout = ({ children }) => (
   <div className="flex flex-col min-h-screen">
     <Navbar />
     <main className="flex-grow">
-    <script src="http://localhost:8097"></script>
       {children}
     </main>
     <Footer />
@@ -29,7 +25,7 @@ const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Routes with MainLayout */}
+        {/* Home Page */}
         <Route 
           path="/" 
           element={
@@ -39,8 +35,7 @@ const AppRoutes = () => {
           } 
         />
         
-        {/* Add more routes as your application grows */}
-        {/* Route:
+        {/* About Page */}
         <Route 
           path="/about" 
           element={
@@ -50,18 +45,27 @@ const AppRoutes = () => {
           } 
         />
         
+        {/* Converter Page */}
         <Route 
-          path="/contact" 
+          path="/converter" 
           element={
             <MainLayout>
-              <Contact />
+              <Converter />
             </MainLayout>
           } 
         />
-        }
+        
+        {/* Minifier Page */}
+        <Route 
+          path="/minifier" 
+          element={
+            <MainLayout>
+              <Minifier />
+            </MainLayout>
+          } 
+        />
         
         {/* Route for handling 404 Not Found */}
-        { 
         <Route 
           path="*" 
           element={
@@ -70,10 +74,9 @@ const AppRoutes = () => {
             </MainLayout>
           } 
         /> 
-        }
         
-        {/* Alternatively, redirect to home page if no matching route */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        {/* Note: This route will never be reached because of the catch-all route above */}
+        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
       </Routes>
     </BrowserRouter>
   );
