2db8f739e665e1c5310e52ccde6063c6f011bd75
BleckWolf25 <joao.coutinho08@icloud.com>
2025-03-11 21:51:49 +0000

    Updated NavBar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,4 @@
 // src/components/layout/Navbar.jsx
-
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import NavButton from '../common/NavButton';
