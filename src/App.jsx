811136896f32f54a4115cee4b1ba3a4a2fd34091
BleckWolf25 <joao.coutinho08@icloud.com>
2025-03-14 19:29:50 +0000

    Remove unecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import AppRoutes from './routes/AppRoutes';
 // App component is now primarily responsible for initializing the router and any global context providers or configurations
 function App() {
   return (
-    // You could wrap AppRoutes with context providers here
-    // <SomeContextProvider>
-    //   <AnotherContextProvider>
     <AppRoutes />
-    //   </AnotherContextProvider>
-    // </SomeContextProvider>
   );
 }
 
